5e2198f4936eef7b4e1088de0e55441f5b733585
Kennedy Odhiambo <x.johnkennedy.x@gmail.com>
2024-05-13 09:32:26 +0300

    maximumHappinessSum

--- src/dsa/May9.ts
+++ src/dsa/May9.ts
@@ -2,23 +2,15 @@ function maximumHappinessSum(happiness: number[], k: number): number {
   const values: Array<number> = []
 
   for (let i = 0; i < k; i++) {
-    console.log("instance of Happiness:", happiness)
     let maxValue = Math.max(...happiness)
     values.push(maxValue)
     const indexOfMaxValue = happiness.indexOf(maxValue)
-    console.log(indexOfMaxValue)
-    happiness = happiness.splice(indexOfMaxValue, 1).map((value) => value - 1)
-
-    // happiness = happiness.map((value) =>
-    //   value !== maxValue ? Math.max(value - 1, 0) : 0
-    // )
+    const withMaxValueRemoved = happiness.splice(indexOfMaxValue, 1)
+    happiness = withMaxValueRemoved.map((value) => Math.max(value - 1, 0))
   }
+  const results = values.reduce((initial, value) => initial + value, 0)
 
-  console.log("values:", values)
-
-  const finalResults = values.reduce((initial, value) => initial + value, 0)
-
-  return finalResults
+  return results
 }
 
 console.log(maximumHappinessSum([10, 47, 47], 3))
